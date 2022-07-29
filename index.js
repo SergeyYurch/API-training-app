3f3c327f75a5a1c1574b7363d8c106ada1d1cb9e
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-07-29 16:35:52 +0300

    3. Added simpler route and try create middleware

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-	res.send('Hello!');
-})
+app.all('/hello', (req, res, next) => {
+	console.log('Middleware is work');
+	next();
+});
+
+app.route('/hello')
+	.get((req, res) => {
+		res.send('Hello!')
+	})
+	.post((req, res) => {
+		res.send('Hello! Post!')
+	})
 
 app.listen(port, () => {
 	console.log(`Серевер за пущен на http://localhost: ${port}`)
