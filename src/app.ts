cc3c9142389879a3cfed2e487752e642e3b4bef1
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-08-04 13:33:15 +0300

    Error handling implementation

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,38 @@ import express, { Express } from 'express';
 import { Server } from 'http'
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
+import { ExeptionFilter } from './common/errors/exeption.filter';
 
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
 	logger: LoggerService;
-	UserController: UserController
+	UserController: UserController;
+	exeptionFilter: ExeptionFilter;
 
-	constructor(logger: LoggerService, UserController: UserController) {
+	constructor(
+		logger: LoggerService,
+		UserController: UserController,
+		exeptionFilter: ExeptionFilter
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
 		this.UserController = UserController;
+		this.exeptionFilter = exeptionFilter;
 	}
 	useRoutes() {
 		this.app.use('/users', this.UserController.router);
 	}
 
+	useExeptionFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
+	}
+
 	public async init() {
 		this.useRoutes();
+		this.useExeptionFilters();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Серевер запущен на http://localhost: ${this.port}`);
 	}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { BaseController } from '../common/base.controller';
 import { LoggerService } from './../logger/logger.service';
+import { HTTPError } from './../common/errors/http-error.class';
 
 export class UserController extends BaseController {
 	constructor(logger: LoggerService) {
@@ -11,7 +12,7 @@ export class UserController extends BaseController {
 		])
 	}
 	login(req: Request, res: Response, next: NextFunction) {
-		this.ok(res, 'login')
+		next(new HTTPError(401, 'ошибка авторизации'));
 	}
 
 	register(req: Request, res: Response, next: NextFunction) {
