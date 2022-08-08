1924ec8f7baf744af7d9ebf68def61db8df4c56a
Sergey Yurchenko <sergey.yurchenko.art@gmail.com>
2022-08-08 16:15:49 +0300

    Added midleware for userController. Implementation validation users data. Used Class-Transformer & Class-Validator

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Email is wrong' })
 	email: string;
+
+	@IsString({ message: 'Password is wrong' })
 	password: string;
+
+	@IsString({ message: 'Name is wrong' })
 	name: string;
 }
